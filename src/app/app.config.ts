944e8d5009cfa83211769a29353b4d8e48ee7de6
Valentina Prado <valenpschool@gmail.com>
2025-01-21 23:14:30 -0300

    chore: primeng e tema da página

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), providePrimeNG({
+    theme: {
+      preset: Aura
+    }
+  })]
 };
