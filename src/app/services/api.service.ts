e16ca6ad792fcd3ecf67db86750e42a5aed159f2
Valentina Prado <valenpschool@gmail.com>
2025-01-23 08:27:43 -0300

    feat: service de integração com API

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
-
+import {HttpBackend, HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
+type ApiReturn = {
+  data: {
+    [key: string]: {
+      "value": string
+      "count": number
+    }
+  }
+}
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  url = "https://panoramads-api.onrender.com/"
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getResponses(): Observable<Object> {
+    return this.http.get(`${this.url}`)
+  }
+
 }
