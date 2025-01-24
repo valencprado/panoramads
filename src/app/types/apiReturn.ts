9a49cf475eff999d562ca0df181fb79e3c96dd75
Valentina Prado <valenpschool@gmail.com>
2025-01-23 23:38:57 -0300

    feat: tipagem

---
+++ src/app/types/apiReturn.ts
@@ -0,0 +1,79 @@
+export type ApiReturn = {
+  data: Data;
+}
+
+export type Data = {
+  idade: [
+    {
+      value: string;
+      count: number;
+    }
+  ];
+  genero: [
+    {
+      value: string;
+      count: number;
+    }
+  ];
+  semestre: [
+    {
+      value: string;
+      count: number;
+    }
+  ];
+  atuacao_atual: [
+    {
+      value: string;
+      count: number;
+    }
+  ];
+  modalidade_trabalho: [
+    {
+      value: string;
+      count: number;
+    }
+  ];
+  regiao_trabalho: [
+    {
+      value: string;
+      count: number;
+    }
+  ];
+  regiao: [
+    {
+      value: string;
+      count: number;
+    }
+  ];
+  porte_empresa: [
+    {
+      value: string;
+      count: number;
+    }
+  ];
+  area_atual: [
+    {
+      value: string;
+      count: number;
+    }
+  ];
+  area_desejo: [
+    {
+      value: string;
+      count: number;
+    }
+  ];
+  satisfacao_trabalho: [
+    {
+      value: string;
+      count: number;
+    }
+  ];
+  dificuldade_trabalho: [
+    {
+      value: string;
+      count: number;
+    }
+  ];
+};
+
