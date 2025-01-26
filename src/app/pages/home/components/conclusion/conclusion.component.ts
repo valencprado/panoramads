f4fce5ef849e7374a1e892c5ad450879a1a8221f
Valentina Prado <valenpschool@gmail.com>
2025-01-25 22:16:04 -0300

    feat: conclusão da pesquisa

---
+++ src/app/pages/home/components/conclusion/conclusion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conclusion',
+  standalone: true,
+  imports: [],
+  templateUrl: './conclusion.component.html',
+  styleUrl: './conclusion.component.scss'
+})
+export class ConclusionComponent {
+
+}
