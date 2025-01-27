34934e773a6a903a4aa4097b6fc86baa7fb6d2a6
Valentina C. Prado <85965282+valencprado@users.noreply.github.com>
2025-01-27 16:59:19 -0300

    Karma config

---
+++ karma.config.js
@@ -0,0 +1,12 @@
+module.exports = function (config) {
+  config.set({
+    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage']
+      }
+    },
+    singleRun: true
+  });
+};
